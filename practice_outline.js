a37aa9303d31e202129211898e666ff0966e3d85
Yasira Muganga <myasira2020@gmail.com>
2023-10-02 18:53:13 +0300

    first push

---
+++ practice_outline.js
@@ -0,0 +1,89 @@
+/*   ----- 02/10/2023
+COURSE OUTLINE --- YUSUF
+1. Variables
+2. Data Types
+3. Functions
+4. Objects
+5. Classes
+6. APIs 
+7. Promises, Async, await
+8. Data Structures & Algorithms in JS 
+9. Real-Life Projects
+*/
+
+/* 
+COURSE OUTLINE --- KALIM
+1. Data Structures & Algorithms
+2. Arrays
+3. Objects
+4. Classes
+5. APIs 
+6. Deployment
+*/
+
+/* 
+COURSE OUTLINE --- YASIRA
+
+PHASE I: 
+
+1. Variables & Data Types
+2. Arrays
+3. Object Literals
+4. Methods for Strings, arrays, objects, stc
+5. Loops - for, while, for...of, forEach, map
+6. Conditionals (If, ternary & Switch)
+7. Functions (normal & arrow)
+8. OOP (Prototypes & Classes)
+9. DOM Selection & Manipulation
+10. Events
+11. Validations --- Forms , etc 
+12. Async, promises, callback
+13. BOM
+14. Web APIs
+
+//  Practice projects --- 3
+
+
+15. AJAX
+16. JSON
+17. JQuery
+
+//   Practice Projects --- 3
+
+18. JS Graphics
+19. Error handling
+20. Generator, advanced iterations
+//   Practice Project ---1 
+21. Modules
+22. Code Quality 
+
+//  Advanced Practice Project --- 1
+
+
+PHASE II:
+1. JS Front -End Frameworks
+//  Practice Projects --- 2
+
+2. JS Backend Frameworks
+
+// Practice Projects --- 2
+
+PHASE III:
+
+1. APIS -- GraphQL & RESTFUL
+//  practice projects --- 2
+
+2. Deployment - CI/CD 
+//  deploy the projects above
+
+PHASE IV:
+1. Advanced Rehearsal of Data Structures & Algorithms 
+
+//  take a couple coding challenge questions
+
+PHASE V:
+1. Behavioural Interview Questions
+
+------ END ------
+
+*/
