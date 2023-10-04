15f4215990bcaab324306d80e08fea75549b3060
Yasira Muganga <myasira2020@gmail.com>
2023-10-04 19:57:54 +0300

    first push

--- practice_outline.js
+++ practice_outline.js
@@ -86,3 +86,39 @@ PHASE V:
 ------ END ------
 
 */
+
+
+/*
+Create a function truncate(str, maxlength) that checks the length of the str and, if it exceeds maxlength – replaces the end of str with the ellipsis character "…", to make its length equal to maxlength.
+
+The result of the function should be the truncated (if needed) string.
+
+For instance:
+truncate("What I'd like to tell on this topic is:", 20) = "What I'd like to te…"
+
+truncate("Hi everyone!", 20) = "Hi everyone!"
+
+SOLUTION:
+
+function truncate(str, maxlength){
+  alert( str.length > maxlength ? str.slice(0,maxlength-1) + '...' : str)
+}
+
+truncate("What I'd like to tell on this topic is:",20);
+
+
+
+Given a string S , return the "reversed"  string where all characters that are not a letter stay in the same place , and all letters reverse their positions
+
+Example 1:
+input : ab-cd
+output: dc-ba
+
+input: a-bC-dEf=ghIj!!
+output: j-Ih-gfE=dCba!! 
+
+
+ 
+
+
+*/
